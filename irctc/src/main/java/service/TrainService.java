aa996b81c00ed29fa2928230bb0e848b9a383d06
angKafka <angdevlab@outlook.com>
2025-04-19 20:09:54 +0530

    Completed half 50%.

--- irctc/src/main/java/service/TrainService.java
+++ irctc/src/main/java/service/TrainService.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import enteties.Train;
-import enteties.User;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TrainService {
     private static final String TRAINS_PATH = "irctc/src/main/java/localDB/trains.json";
@@ -49,5 +50,42 @@ public class TrainService {
 
         return sourceIndex != -1 && destinationIndex != -1 && sourceIndex < destinationIndex;
     }
+
     //Seats available Train
+    public List<List<Integer>> getSeats(String  trainId){
+        return trains.stream().filter(train1 -> train1.getTrainId().equals(trainId)).map(Train::getSeats).findFirst().orElse(null);
+    }
+
+    //Fetch train using Id
+    public Train getTrain(String trainId){
+        return trains.stream().filter(train1 -> train1.getTrainId().equals(trainId)).findFirst().orElse(null);
+    }
+
+    //Adding Train
+    public void saveTrain(Train newTrain) throws IOException {
+        Optional<Train> existingTrain = trains.stream().filter(t -> t.getTrainId().equalsIgnoreCase(newTrain.getTrainId())).findFirst();
+        if (existingTrain.isPresent()) {
+            updateTrain(newTrain);
+        }else{
+            trains.add(newTrain);
+            saveTrainListToFile();
+        }
+    }
+
+
+    //Update train
+    public void updateTrain(Train updateTrain) throws IOException {
+        OptionalInt index = IntStream.range(0, trains.size()).filter(i -> trains.get(i).getTrainId().equalsIgnoreCase(updateTrain.getTrainId())).findFirst();
+
+        if (index.isPresent()) {
+            trains.set(index.getAsInt(), updateTrain);
+            saveTrainListToFile();
+        }else{
+            saveTrain(updateTrain);
+        }
+    }
+
+    private void saveTrainListToFile() throws IOException {
+        objectMapper.writeValue(new File(TRAINS_PATH), trains); // Write the updated list back to the file
+    }
 }
